23b761a16ef9121808aa93eddbd55b0fd1a5f09d
admin <798180744@qq.com>
2021-06-06 12:22:52 +0800

    milestone 1

---
+++ app/src/main/java/com/example/timaster/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.timaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn=findViewById(R.id.btn);
+        final Uri uri=Uri.parse("https://luminus.nus.edu.sg/");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+    }
+}
